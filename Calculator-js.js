0b3bad6d61ed13369cc1ddb43a308cd78fc488ef
Den <akosimendiola@gmail.com>
2022-06-21 23:32:08 +0800

    adasdfsf

--- Calculator-js.js
+++ Calculator-js.js
@@ -8,7 +8,7 @@
 function displayValue(value) 
 {
     // This statements appends values to the display box as the user clicked number and operator buttons
-    document.getElementById("tanginam").value += value;
+    document.getElementById("tanginamerzozozo").value += value;
 }
 
 function calculateNum() 
