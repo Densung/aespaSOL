05a5affd29f5e195b48df16ea9bb5bfc9abf3f16
May <maygarin06@gmail.com>
2022-06-21 23:56:21 +0800

    weeeejdsas

--- Calculator-js.js
+++ Calculator-js.js
@@ -8,7 +8,7 @@
 function displayValue(value) 
 {
     // This statements appends values to the display box as the user clicked number and operator buttons
-    document.getElementById("tanginamerzozozo").value += value;
+    document.getElementById("RATATATATTA").value += value;
 
 }
 
