c172c86df45b64bceefbb5e7364c31c0a3a520d4
Den <akosimendiola@gmail.com>
2022-06-23 08:43:18 +0800

    init

--- Home-js.js
+++ Home-js.js
@@ -1,15 +1,15 @@
-document.getElementById("smcu-updates1").innerHTML = "(June 22, 2022)<br>SMCU Ep. 2 Behind the Scenes!";
+document.getElementById("smcu-updates1").innerHTML = "<u>(June 22, 2022)</u><br>SMCU Ep. 2 Behind the Scenes!";
 document.getElementById("smcu-updates2").innerHTML = 
-"(June 22, 2022)<br>"+
+"<u>(June 22, 2022)</u><br>"+
 "aespa 에스파 〖Life’s Too Short〗<br>" +
 "➫ 2022.06.24 1PM KST<br>" +
 "#aespa #æspa #에스파<br>" + 
 "#LifesTooShort #LTS #aespa_LTS<br>" +
 "#Girls #aespa_Girls";
-document.getElementById("smcu-updates3").innerHTML = "(June 20, 2022) <br>SMCU Ep. 2 is Out Now!";
+document.getElementById("smcu-updates3").innerHTML = "<u>(June 20, 2022)</u> <br>SMCU Ep. 2 is Out Now!";
 document.getElementById("ig-updates").innerHTML = "Instagram Updates";
 document.getElementById("igu1").innerHTML = 
-"(June 22, 2022)<br>"+
+"<u>(June 22, 2022)</u><br>"+
 "#광고 @official_CLIO #클리오 #쉬폰블러틴트<br>" + 
 "#aespa #KARINA #GISELLE";
-document.getElementById("igu2").innerHTML = "(June 22, 2022)<br>"+"씨유쑨!!!🤍☃️";
+document.getElementById("igu2").innerHTML = "<u>(June 22, 2022)</u><br>"+"씨유쑨!!!🤍☃️";
