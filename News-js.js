46f0f754cfcf0a08ea23e4ef2f65070d92af3618
Den <akosimendiola@gmail.com>
2022-06-23 15:49:27 +0800

    news

--- News-js.js
+++ News-js.js
@@ -7,13 +7,13 @@ function readMore()
     if (dot.style.display === "none") 
     {
       dot.style.display = "inline";
-      buttonText.innerHTML = "See more"; 
+      buttonText.innerHTML = "<u>See more</u>"; 
       moreText.style.display = "none";
     } 
     else 
     {
       dot.style.display = "none";
-      buttonText.innerHTML = "See less"; 
+      buttonText.innerHTML = "<u>See less</u>"; 
       moreText.style.display = "inline";
     }
 }
@@ -26,13 +26,89 @@ function readMore1()
     if (dot.style.display === "none") 
     {
       dot.style.display = "inline";
-      buttonText.innerHTML = "See more"; 
+      buttonText.innerHTML = "<u>See more</u>"; 
       moreText.style.display = "none";
     } 
     else 
     {
       dot.style.display = "none";
-      buttonText.innerHTML = "See less"; 
+      buttonText.innerHTML = "<u>See less</u>"; 
+      moreText.style.display = "inline";
+    }
+}
+function readMore2() 
+{
+    var dot = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var buttonText = document.getElementById("readButton2");
+  
+    if (dot.style.display === "none") 
+    {
+      dot.style.display = "inline";
+      buttonText.innerHTML = "<u>See more</u>"; 
+      moreText.style.display = "none";
+    } 
+    else 
+    {
+      dot.style.display = "none";
+      buttonText.innerHTML = "<u>See less</u>"; 
+      moreText.style.display = "inline";
+    }
+}
+function readMore3() 
+{
+    var dot = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var buttonText = document.getElementById("readButton3");
+  
+    if (dot.style.display === "none") 
+    {
+      dot.style.display = "inline";
+      buttonText.innerHTML = "<u>See more</u>"; 
+      moreText.style.display = "none";
+    } 
+    else 
+    {
+      dot.style.display = "none";
+      buttonText.innerHTML = "<u>See less</u>"; 
+      moreText.style.display = "inline";
+    }
+}
+function readMore4() 
+{
+    var dot = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var buttonText = document.getElementById("readButton4");
+  
+    if (dot.style.display === "none") 
+    {
+      dot.style.display = "inline";
+      buttonText.innerHTML = "<u>See more</u>"; 
+      moreText.style.display = "none";
+    } 
+    else 
+    {
+      dot.style.display = "none";
+      buttonText.innerHTML = "<u>See less</u>"; 
+      moreText.style.display = "inline";
+    }
+}
+function readMore5() 
+{
+    var dot = document.getElementById("dots5");
+    var moreText = document.getElementById("more5");
+    var buttonText = document.getElementById("readButton5");
+  
+    if (dot.style.display === "none") 
+    {
+      dot.style.display = "inline";
+      buttonText.innerHTML = "<u>See more</u>"; 
+      moreText.style.display = "none";
+    } 
+    else 
+    {
+      dot.style.display = "none";
+      buttonText.innerHTML = "<u>See less</u>"; 
       moreText.style.display = "inline";
     }
 }
